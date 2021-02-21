9596c5c85db130f9b2104d212fa78216b2588eff
Peter Birch <peter@lightlogic.co.uk>
2021-02-21 18:54:04 +0000

    __init__.py: Using Rich logging handler if available

--- compiler/__init__.py
+++ compiler/__init__.py
@@ -11,3 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import logging
+
+# If available, use Rich to colourise the output log
+try:
+    from rich.logging import RichHandler
+    logging.basicConfig(
+        level="NOTSET", format="%(message)s", datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True)]
+    )
+except ModuleNotFoundError:
+    pass
