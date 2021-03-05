e1ee576c27cb4895c6c1d11f351233d6d25713c9
Peter Birch <peter@lightlogic.co.uk>
2021-03-05 17:33:47 +0000

    RTL - nx_node: Integrating control block with command streams

--- hardware/rtl/nx_node.sv
+++ hardware/rtl/nx_node.sv
@@ -14,12 +14,13 @@
 
 module nx_node #(
     // Command interface parameters
-      parameter CMD_W  =  8
-    // Node core parameters
-    , parameter OP_W   =  4 // Operand encoding width
-    , parameter REG_W  = 16 // Internal register width
-    , parameter IO_W   =  4 // Primary inputs/outputs width
-    , parameter SLOTS  = 32 // How many instruction slots are available
+      parameter TARGET_W =  8 // Address width for targets
+    , parameter CMD_W    =  8 // Command bus width
+    // Logic core parameters
+    , parameter OP_W     =  4 // Operand encoding width
+    , parameter REG_W    = 16 // Internal register width
+    , parameter IO_W     =  4 // Primary inputs/outputs width
+    , parameter SLOTS    = 32 // How many instruction slots are available
 ) (
       input  logic clk
     , input  logic rst
@@ -41,10 +42,76 @@ module nx_node #(
     , input  logic             tx_cmd_ready
 );
 
+localparam PAYLOAD_W = 24;
+localparam INST_W    = OP_W + (3 * $clog2(REG_W)) + 1 + $clog2(IO_W);
+
+logic m_stall;
+
+logic [            CMD_W-1:0] m_rx_command;
+logic [        PAYLOAD_W-1:0] m_rx_payload;
+logic [(PAYLOAD_W/CMD_W)-1:0] m_rx_valid;
+logic                         m_rx_complete;
+logic                         m_rx_ready;
+
+logic [       INST_W-1:0] m_load_instr;
+logic [$clog2(SLOTS)-1:0] m_load_slot;
+logic                     m_load_last;
+logic                     m_load_valid;
+
+logic                    m_in_value;
+logic [$clog2(IO_W)-1:0] m_in_index;
+logic                    m_in_valid;
+
+logic [IO_W-1:0] m_out_values;
+logic [IO_W-1:0] m_out_valids;
+
+logic [         TARGET_W-1:0] m_tx_target;
+logic [            CMD_W-1:0] m_tx_command;
+logic [        PAYLOAD_W-1:0] m_tx_payload;
+logic [(PAYLOAD_W/CMD_W)-1:0] m_tx_valid;
+logic                         m_tx_ready;
+
+// Controller
+nx_node_control #(
+      .TARGET_W (TARGET_W )
+    , .CMD_W    (CMD_W    )
+    , .PAYLOAD_W(PAYLOAD_W)
+) m_control (
+      .clk(clk)
+    , .rst(rst)
+    // Core control
+    , .stall(m_stall)
+    // Receive interface
+    , .rx_command (m_rx_command )
+    , .rx_payload (m_rx_payload )
+    , .rx_valid   (m_rx_valid   )
+    , .rx_complete(m_rx_complete)
+    , .rx_ready   (m_rx_ready   )
+    // Instruction load
+    , .load_instr(m_load_instr)
+    , .load_slot (m_load_slot )
+    , .load_last (m_load_last )
+    , .load_valid(m_load_valid)
+    // Input load
+    , .in_value(m_in_value)
+    , .in_index(m_in_index)
+    , .in_valid(m_in_valid)
+    // Value output
+    , .out_values(m_out_values)
+    , .out_valids(m_out_valids)
+    // Transmit interface
+    , .tx_target (m_tx_target )
+    , .tx_command(m_tx_command)
+    , .tx_payload(m_tx_payload)
+    , .tx_valid  (m_tx_valid  )
+    , .tx_ready  (m_tx_ready  )
+);
+
 // Receive instructions and input signal state
 nx_receiver #(
-      .BUS_W    (CMD_W)
-    , .PAYLOAD_W(   24)
+      .TARGET_W ( TARGET_W)
+    , .BUS_W    (    CMD_W)
+    , .PAYLOAD_W(PAYLOAD_W)
 ) m_cmd_rx (
       .clk(clk)
     , .rst(rst)
@@ -54,10 +121,11 @@ nx_receiver #(
     , .cmd_valid(rx_cmd_valid)
     , .cmd_ready(rx_cmd_ready)
     // Data received
-    , .rx_command()
-    , .rx_payload()
-    , .rx_valid  ()
-    , .rx_ready  ()
+    , .rx_command (m_rx_command )
+    , .rx_payload (m_rx_payload )
+    , .rx_valid   (m_rx_valid   )
+    , .rx_complete(m_rx_complete)
+    , .rx_ready   (m_rx_ready   )
 );
 
 // Execute logic code
@@ -70,36 +138,39 @@ nx_node_core #(
       .clk(clk)
     , .rst(rst)
     // External controls
-    , .tick(tick)
+    , .tick (tick   )
+    , .stall(m_stall)
     // State outputs
     , .in_setup(in_setup)
     , .in_wait (in_wait )
     , .in_run  (in_run  )
     // Instruction load
-    , .load_instr()
-    , .load_slot ()
-    , .load_valid()
+    , .load_instr(m_load_instr)
+    , .load_slot (m_load_slot )
+    , .load_last (m_load_last )
+    , .load_valid(m_load_valid)
     // Input load
-    , .in_value()
-    , .in_index()
-    , .in_valid()
+    , .in_value(m_in_value)
+    , .in_index(m_in_index)
+    , .in_valid(m_in_valid)
     // Value output
-    , .out_values()
-    , .out_valids()
+    , .out_values(m_out_values)
+    , .out_valids(m_out_valids)
 );
 
 // Transmit output signal state
 nx_transmitter #(
-      .BUS_W    (CMD_W)
-    , .PAYLOAD_W(   24)
+      .TARGET_W ( TARGET_W)
+    , .BUS_W    (    CMD_W)
+    , .PAYLOAD_W(PAYLOAD_W)
 ) m_cmd_tx (
       .clk(clk)
     , .rst(rst)
     // Data to send
-    , .tx_command()
-    , .tx_payload()
-    , .tx_valid  ()
-    , .tx_ready  ()
+    , .tx_command(m_tx_command)
+    , .tx_payload(m_tx_payload)
+    , .tx_valid  (m_tx_valid  )
+    , .tx_ready  (m_tx_ready  )
     // Command interface
     , .cmd_data (tx_cmd_data )
     , .cmd_last (tx_cmd_last )
