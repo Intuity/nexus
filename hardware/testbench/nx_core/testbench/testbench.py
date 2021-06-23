f412e3d166afa8a8f4a3d51f451b5813b1f850be
Peter Birch <peter@lightlogic.co.uk>
2021-06-23 16:50:34 +0100

    Hardware - Testbench - nx_node: Starting to build a bench for the core

---
+++ hardware/testbench/nx_core/testbench/testbench.py
@@ -0,0 +1,72 @@
+# Copyright 2021, Peter Birch, mailto:peter@lightlogic.co.uk
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import cocotb
+from cocotb.triggers import ClockCycles, RisingEdge
+from cocotb_bus.scoreboard import Scoreboard
+
+from tb_base import TestbenchBase
+from drivers.io_common import IORole
+from drivers.stream.io import StreamIO
+from drivers.stream.init import StreamInitiator
+from drivers.stream.resp import StreamResponder
+
+class Testbench(TestbenchBase):
+
+    def __init__(self, dut):
+        """ Initialise the testbench.
+
+        Args:
+            dut: Pointer to the DUT
+        """
+        super().__init__(dut)
+        # Setup drivers/monitors
+        self.inbound = [
+            StreamInitiator(
+                self, self.clk, self.rst,
+                StreamIO(self.dut, dirx, IORole.RESPONDER)
+            ) for dirx in ("ib_north", "ib_east", "ib_south", "ib_west")
+        ]
+        self.outbound = [
+            StreamResponder(
+                self, self.clk, self.rst,
+                StreamIO(self.dut, dirx, IORole.INITIATOR), name=dirx,
+            ) for dirx in ("ob_north", "ob_east", "ob_south", "ob_west")
+        ]
+        # Create expected outbound queues
+        self.expected = [[] for _ in self.outbound]
+        # Create a scoreboard
+        self.scoreboard = Scoreboard(self, fail_immediately=False)
+        for resp, exp in zip(self.outbound, self.expected):
+            self.scoreboard.add_interface(resp, exp, reorder_depth=100)
+
+    async def initialise(self):
+        """ Initialise the DUT's I/O """
+        await super().initialise()
+        for init in self.inbound : init.intf.initialise(IORole.INITIATOR)
+        for resp in self.outbound: resp.intf.initialise(IORole.RESPONDER)
+        self.trigger_i  <= 0
+        self.node_row_i <= 0
+        self.node_col_i <= 0
+
+class testcase(cocotb.test):
+    def __call__(self, dut, *args, **kwargs):
+        async def __run_test():
+            tb = Testbench(dut)
+            await self._func(tb, *args, **kwargs)
+            for idx, exp in enumerate(tb.expected):
+                while exp: await RisingEdge(tb.clk)
+                tb.info(f"Exhausted queue {idx}")
+            raise tb.scoreboard.result
+        return cocotb.decorators.RunningTest(__run_test(), self)
---
+++ hardware/testbench/nx_core/testbench/testcases/routing.py
@@ -0,0 +1,109 @@
+# Copyright 2021, Peter Birch, mailto:peter@lightlogic.co.uk
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from random import choice, randint
+
+from nx_constants import Command, Direction
+from nx_message import (build_load_instr, build_map_input, build_map_output,
+                        build_sig_state)
+
+from ..testbench import testcase
+
+@testcase()
+async def routing(dut):
+    """ Exercise message routing through a node """
+    # Reset the DUT
+    dut.info("Resetting the DUT")
+    await dut.reset()
+
+    # Setup a row & column
+    row, col = randint(1, 14), randint(1, 14)
+    dut.info(f"Setting row to {row} & column to {col}")
+    dut.node_row_i <= row
+    dut.node_col_i <= col
+
+    for _ in range(1000):
+        # Choose a random command type
+        command = choice(list(Command))
+
+        # Select a target row and column
+        broadcast = choice((0, 1))
+        bc_decay  = randint(1, 10)
+        tgt_row, tgt_col = row, col
+        if choice((0, 1)):
+            tgt_row, tgt_col = randint(0, 15), randint(0, 15)
+
+        # Check if this message is targeted at the node
+        tgt_match = (tgt_row == row) and (tgt_col == col)
+
+        # Generate a message
+        msg = 0
+        if command == Command.LOAD_INSTR:
+            msg = build_load_instr(
+                broadcast, tgt_row, tgt_col, bc_decay, choice((0, 1)),
+                randint(0, (1 << 15) - 1),
+            )
+        elif command == Command.INPUT:
+            msg = build_map_input(
+                broadcast, tgt_row, tgt_col, bc_decay, randint(0, 7),
+                choice((0, 1)), randint(0, 15), randint(0, 15), randint(0, 7),
+            )
+        elif command == Command.OUTPUT:
+            msg = build_map_output(
+                broadcast, tgt_row, tgt_col, bc_decay, randint(0,  7),
+                choice((0, 1)), choice((0, 1)), randint(0, 15), randint(0, 15),
+            )
+        elif command == Command.SIG_STATE:
+            msg = build_sig_state(
+                broadcast, tgt_row, tgt_col, bc_decay, choice((0, 1)),
+                randint(0, 15), randint(0, 15), randint(0,  7),
+            )
+
+        # Create a message
+        dut.debug(
+            f"MSG - BC: {broadcast}, R: {tgt_row}, C: {tgt_col}, CMD: {command} "
+            f"-> 0x{msg:08X}"
+        )
+
+        # Generate bypass message
+        byp_msg = msg
+        if broadcast:
+            byp_msg &= 0x807F_FFFF
+            byp_msg |= (bc_decay - 1) << 23
+
+        # Queue up the message
+        in_pipe = choice(dut.inbound)
+        in_dirx = Direction(dut.inbound.index(in_pipe))
+        in_pipe.append(msg)
+
+        # If not matching this target, expect message to be routed elsewhere
+        if broadcast:
+            if in_dirx == Direction.NORTH:
+                dut.expected[int(Direction.EAST )].append((byp_msg, 0))
+                dut.expected[int(Direction.SOUTH)].append((byp_msg, 0))
+                dut.expected[int(Direction.WEST )].append((byp_msg, 0))
+            elif in_dirx == Direction.EAST:
+                dut.expected[int(Direction.WEST )].append((byp_msg, 0))
+            if in_dirx == Direction.SOUTH:
+                dut.expected[int(Direction.NORTH)].append((byp_msg, 0))
+                dut.expected[int(Direction.EAST )].append((byp_msg, 0))
+                dut.expected[int(Direction.WEST )].append((byp_msg, 0))
+            elif in_dirx == Direction.WEST:
+                dut.expected[int(Direction.EAST )].append((byp_msg, 0))
+        elif not tgt_match:
+            if   tgt_row < row: tgt_dirx = Direction.NORTH
+            elif tgt_row > row: tgt_dirx = Direction.SOUTH
+            elif tgt_col < col: tgt_dirx = Direction.WEST
+            elif tgt_col > col: tgt_dirx = Direction.EAST
+            dut.expected[int(tgt_dirx)].append((byp_msg, 0))
