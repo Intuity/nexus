aa9898434628d617163179d149f4c09b7cf8689c
Peter Birch <peter@lightlogic.co.uk>
2021-06-23 18:25:17 +0100

    Hardware - RTL - nx_stream_distributor: Using presence flags to divert messages

--- hardware/rtl/nx_stream_distributor.sv
+++ hardware/rtl/nx_stream_distributor.sv
@@ -32,41 +32,62 @@ module nx_stream_distributor #(
     , output logic [STREAM_WIDTH-1:0] north_data_o
     , output logic                    north_valid_o
     , input  logic                    north_ready_i
+    , input  logic                    north_present_i
     // - East
     , output logic [STREAM_WIDTH-1:0] east_data_o
     , output logic                    east_valid_o
     , input  logic                    east_ready_i
+    , input  logic                    east_present_i
     // - South
     , output logic [STREAM_WIDTH-1:0] south_data_o
     , output logic                    south_valid_o
     , input  logic                    south_ready_i
+    , input  logic                    south_present_i
     // - West
     , output logic [STREAM_WIDTH-1:0] west_data_o
     , output logic                    west_valid_o
     , input  logic                    west_ready_i
+    , input  logic                    west_present_i
 );
 
 // Constants and enumerations
 `include "nx_constants.svh"
 
 // Construct outputs
+// - If NORTH absent route EAST
+// - If EAST  absent route SOUTH
+// - If SOUTH absent route WEST
+// - If WEST  absent route NORTH
+
 assign north_data_o  = dist_data_i;
-assign north_valid_o = dist_valid_i && (dist_dir_i == DIRX_NORTH);
+assign north_valid_o = dist_valid_i && (
+    (dist_dir_i == DIRX_NORTH &&  north_present_i) ||
+    (dist_dir_i == DIRX_WEST  && !west_present_i )
+);
 
 assign east_data_o   = dist_data_i;
-assign east_valid_o  = dist_valid_i && (dist_dir_i == DIRX_EAST);
+assign east_valid_o  = dist_valid_i && (
+    (dist_dir_i == DIRX_EAST  &&  east_present_i ) ||
+    (dist_dir_i == DIRX_NORTH && !north_present_i)
+);
 
 assign south_data_o  = dist_data_i;
-assign south_valid_o = dist_valid_i && (dist_dir_i == DIRX_SOUTH);
+assign south_valid_o = dist_valid_i && (
+    (dist_dir_i == DIRX_SOUTH &&  south_present_i) ||
+    (dist_dir_i == DIRX_EAST  && !east_present_i )
+);
 
 assign west_data_o   = dist_data_i;
-assign west_valid_o  = dist_valid_i && (dist_dir_i == DIRX_WEST);
+assign west_valid_o  = dist_valid_i && (
+    (dist_dir_i == DIRX_WEST  &&  west_present_i ) ||
+    (dist_dir_i == DIRX_SOUTH && !south_present_i)
+);
 
 assign dist_ready_o = (
-     (dist_dir_i == DIRX_NORTH) ? north_ready_i :
-    ((dist_dir_i == DIRX_EAST ) ? east_ready_i  :
-    ((dist_dir_i == DIRX_SOUTH) ? south_ready_i :
-                                  west_ready_i))
+     (dist_dir_i == DIRX_NORTH) ? (north_ready_i && north_present_i || east_ready_i  && !north_present_i) :
+    ((dist_dir_i == DIRX_EAST ) ? (east_ready_i  && east_present_i  || south_ready_i && !east_present_i ) :
+    ((dist_dir_i == DIRX_SOUTH) ? (south_ready_i && south_present_i || west_ready_i  && !south_present_i) :
+                                  (west_ready_i  && west_present_i  || north_ready_i && !west_present_i )))
 );
 
 endmodule : nx_stream_distributor
