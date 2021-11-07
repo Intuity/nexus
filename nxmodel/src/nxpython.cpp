d7e9e1a4d97d047e8cc0ef70f78982e9c5990a08
Peter Birch <peter@lightlogic.co.uk>
2021-11-07 21:33:45 +0000

    NXModel - nxpython.cpp: Exporting more functions, structs, and enumerations from C++ to Python

--- nxmodel/src/nxpython.cpp
+++ nxmodel/src/nxpython.cpp
@@ -25,6 +25,71 @@ namespace py = pybind11;
 using namespace NXModel;
 
 PYBIND11_MODULE(nxmodel, m) {
+    // Expose enumerations
+    py::enum_<NXConstants::direction_t>(m, "direction_t")
+        .value("NORTH", NXConstants::DIRECTION_NORTH)
+        .value("EAST",  NXConstants::DIRECTION_EAST )
+        .value("SOUTH", NXConstants::DIRECTION_SOUTH)
+        .value("WEST",  NXConstants::DIRECTION_WEST )
+        .export_values();
+
+    // Expose structs
+    py::class_<NXConstants::node_control_t>(m, "node_control_t")
+        .def(py::init([]() { node_control_t _; return _; }));
+    py::class_<NXConstants::node_load_t>(m, "node_load_t")
+        .def(py::init([]() { node_load_t _; return _; }));
+    py::class_<NXConstants::node_loopback_t>(m, "node_loopback_t")
+        .def(py::init([]() { node_loopback_t _; return _; }));
+    py::class_<NXConstants::node_signal_t>(m, "node_signal_t")
+        .def(py::init([]() { node_signal_t _; return _; }));
+    py::class_<NXConstants::node_raw_t>(m, "node_raw_t")
+        .def(py::init([]() { node_raw_t _; return _; }));
+
+    // Expose packing functions
+    m.def("pack_node_control", [](NXConstants::node_control_t msg) -> uint32_t {
+        uint32_t raw = 0;
+        NXConstants::pack_node_control(msg, (uint8_t *)&raw);
+        return raw;
+    });
+    m.def("pack_node_load", [](NXConstants::node_load_t msg) -> uint32_t {
+        uint32_t raw = 0;
+        NXConstants::pack_node_load(msg, (uint8_t *)&raw);
+        return raw;
+    });
+    m.def("pack_node_loopback", [](NXConstants::node_loopback_t msg) -> uint32_t {
+        uint32_t raw = 0;
+        NXConstants::pack_node_loopback(msg, (uint8_t *)&raw);
+        return raw;
+    });
+    m.def("pack_node_signal", [](NXConstants::node_signal_t msg) -> uint32_t {
+        uint32_t raw = 0;
+        NXConstants::pack_node_signal(msg, (uint8_t *)&raw);
+        return raw;
+    });
+    m.def("pack_node_raw", [](NXConstants::node_raw_t msg) -> uint32_t {
+        uint32_t raw = 0;
+        NXConstants::pack_node_raw(msg, (uint8_t *)&raw);
+        return raw;
+    });
+
+    // Expose unpacking functions
+    m.def("unpack_node_control", [](uint32_t raw) -> NXConstants::node_control_t {
+        return NXConstants::unpack_node_control((uint8_t *)&raw);
+    });
+    m.def("unpack_node_load", [](uint32_t raw) -> NXConstants::node_load_t {
+        return NXConstants::unpack_node_load((uint8_t *)&raw);
+    });
+    m.def("unpack_node_loopback", [](uint32_t raw) -> NXConstants::node_loopback_t {
+        return NXConstants::unpack_node_loopback((uint8_t *)&raw);
+    });
+    m.def("unpack_node_signal", [](uint32_t raw) -> NXConstants::node_signal_t {
+        return NXConstants::unpack_node_signal((uint8_t *)&raw);
+    });
+    m.def("unpack_node_raw", [](uint32_t raw) -> NXConstants::node_raw_t {
+        return NXConstants::unpack_node_raw((uint8_t *)&raw);
+    });
+
+    // Expose classes
     py::class_<Nexus, std::shared_ptr<Nexus>>(m, "Nexus")
         .def(py::init<uint32_t, uint32_t>())
         .def("get_rows",            &Nexus::get_rows           )
@@ -45,6 +110,10 @@ PYBIND11_MODULE(nxmodel, m) {
 
     py::class_<NXNode, std::shared_ptr<NXNode>>(m, "NXNode")
         .def(py::init<uint32_t, uint32_t>())
+        .def("attach",                &NXNode::attach               )
+        .def("get_pipe",              &NXNode::get_pipe             )
+        .def("is_idle",               &NXNode::is_idle              )
+        .def("step",                  &NXNode::step                 )
         .def("get_memory",            &NXNode::get_memory           )
         .def("get_instruction_count", &NXNode::get_instruction_count)
         .def("get_output_count",      &NXNode::get_output_count     )
