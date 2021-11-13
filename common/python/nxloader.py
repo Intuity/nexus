263b71378a489ea3d49b02f21b644629af57a788
Peter Birch <peter@lightlogic.co.uk>
2021-11-13 11:45:49 +0000

    Common - nxloader.py: Updating to support new export format

--- common/python/nxloader.py
+++ common/python/nxloader.py
@@ -12,14 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from collections import namedtuple
-
+from dataclasses import dataclass, field
 import json
 from pathlib import Path
+from typing import List
 
-from nxconstants import NodeCommand, NodeLoadInstr, NodeMapOutput
+from nxconstants import Instruction, OutputLookup, OutputMapping
 
-Loaded = namedtuple("Loaded", ["instructions", "mappings"])
+@dataclass
+class NodeState:
+    instructions : List[Instruction] = field(default_factory=list)
+    loopback     : int = 0
+    lookup       : List[OutputLookup] = field(default_factory=list)
+    mappings     : List[OutputMapping] = field(default_factory=list)
 
 class NXLoader:
     """ Parses JSON designs exported by nxcompile """
@@ -37,15 +42,18 @@ class NXLoader:
     CFG_ND_REGS    = "registers"
     CFG_ND_SLOTS   = "slots"
     # Per-node configuration
-    NODE_ROW    = "row"
-    NODE_COLUMN = "column"
-    NODE_INSTRS = "instructions"
-    NODE_MSGS   = "messages"
-    # Design reports
-    DSG_REP_STATE   = "state"
-    DSG_REP_OUTPUTS = "outputs"
+    NODE_ROW      = "row"
+    NODE_COLUMN   = "column"
+    NODE_INSTRS   = "instructions"
+    NODE_LOOPBACK = "loopback"
+    NODE_OUTPUTS  = "outputs"
+    # Output mapping entries
+    MAPPING_ROW    = "row"
+    MAPPING_COLUMN = "column"
+    MAPPING_INDEX  = "index"
+    MAPPING_IS_SEQ = "is_seq"
 
-    def load(self, path):
+    def __init__(self, path):
         """ Load a design
 
         Args:
@@ -58,38 +66,48 @@ class NXLoader:
         else:
             model = json.load(path)
         # Pickup the configuration section
-        config   = model[NXLoader.DESIGN_CONFIG]
-        cfg_rows = config[NXLoader.CONFIG_ROWS]
-        cfg_cols = config[NXLoader.CONFIG_COLUMNS]
-        # Track what should be loaded to each node
-        instrs   = [[[] for _ in range(cfg_cols)] for _ in range(cfg_rows)]
-        mappings = [[[] for _ in range(cfg_cols)] for _ in range(cfg_rows)]
+        config        = model[NXLoader.DESIGN_CONFIG]
+        self.cfg_rows = config[NXLoader.CONFIG_ROWS]
+        self.cfg_cols = config[NXLoader.CONFIG_COLUMNS]
+        self.cfg_outs = config[NXLoader.CONFIG_NODE][NXLoader.CFG_ND_OUTPUTS]
+        # Track state for each node
+        self.state = [
+            [NodeState() for _ in range(self.cfg_cols)] for _ in range(self.cfg_rows)
+        ]
         # Start loading the compiled design into the mesh
         nodes = model[NXLoader.DESIGN_NODES]
         for node_data in nodes:
             # Get the row and column of the target node
             n_row = node_data[NXLoader.NODE_ROW]
             n_col = node_data[NXLoader.NODE_COLUMN]
-            # Pickup instructions and messages for each node
-            # Decode instructions to be loaded
-            for raw_instr in node_data[NXLoader.NODE_INSTRS]:
-                msg                = NodeLoadInstr()
-                msg.header.row     = n_row
-                msg.header.column  = n_col
-                msg.header.command = NodeCommand.LOAD_INSTR
-                msg.instr.unpack(raw_instr)
-                instrs[n_row][n_col].append(msg)
-            # Decode output mappings to be loaded
-            for idx_output, msgs in enumerate(node_data[NXLoader.NODE_MSGS]):
-                for tgt_row, tgt_col, tgt_idx, tgt_seq in msgs:
-                    msg                = NodeMapOutput()
-                    msg.header.row     = n_row
-                    msg.header.column  = n_col
-                    msg.header.command = NodeCommand.MAP_OUTPUT
-                    msg.source_index   = idx_output
-                    msg.target_row     = tgt_row
-                    msg.target_column  = tgt_col
-                    msg.target_index   = tgt_idx
-                    msg.target_is_seq  = tgt_seq
-                    mappings[n_row][n_col].append(msg)
-        return Loaded(instructions=instrs, mappings=mappings)
+            # Get point to the state object
+            state = self.state[n_row][n_col]
+            # Read in all of the instructions
+            for raw in node_data[NXLoader.NODE_INSTRS]:
+                instr = Instruction()
+                instr.unpack(raw)
+                state.instructions.append(instr)
+            # Pickup the loopback mapping
+            state.loopback = node_data[NXLoader.NODE_LOOPBACK]
+            # Read in all of the output mappings
+            counts = {}
+            for idx, entries in enumerate(node_data[NXLoader.NODE_OUTPUTS]):
+                counts[idx] = len(entries)
+                for entry in entries:
+                    mapping = OutputMapping(
+                        row   =entry[NXLoader.MAPPING_ROW],
+                        column=entry[NXLoader.MAPPING_COLUMN],
+                        index =entry[NXLoader.MAPPING_INDEX],
+                        is_seq=entry[NXLoader.MAPPING_IS_SEQ],
+                    )
+                    state.mappings.append(mapping)
+            # Generate output lookups
+            offset = len(state.instructions) + self.cfg_outs
+            for idx in range(self.cfg_outs):
+                lookup = OutputLookup(
+                    active=(counts.get(idx, 0) > 0),
+                    start =offset,
+                    stop  =(offset + counts.get(idx, 0) - 1),
+                )
+                state.lookup.append(lookup)
+                offset += counts.get(idx, 0)
