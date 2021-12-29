fe788b563ebe6d9825f019f86638a64972efb4b6
Peter Birch <peter@lightlogic.co.uk>
2021-12-29 11:39:46 +0000

    Hardware - RTL - nx_control: Substantial change to control scheme, widening stream interface to 128-bit, generating output messages

--- hardware/rtl/top/nx_control.sv
+++ hardware/rtl/top/nx_control.sv
@@ -27,211 +27,302 @@ import NXConstants::*;
     , parameter OUTPUTS   = 32
     , parameter REGISTERS = 16
 ) (
-      input  logic               i_clk
-    , input  logic               i_rst
-    // Inbound message stream (from host)
-    , input  control_message_t   i_inbound_data
-    , input  logic               i_inbound_valid
-    , output logic               o_inbound_ready
-    // Outbound message stream (to host)
-    , output control_response_t  o_outbound_data
-    , output logic               o_outbound_valid
-    , input  logic               i_outbound_ready
+      input  logic                         i_clk
+    , input  logic                         i_rst
     // Soft reset request
-    , output logic               o_soft_reset
+    , output logic                         o_soft_reset
+    // Host message streams
+    // - Inbound
+    , input  control_request_t             i_ctrl_in_data
+    , input  logic                         i_ctrl_in_valid
+    , output logic                         o_ctrl_in_ready
+    // - Outbound
+    , output control_response_t            o_ctrl_out_data
+    , output logic                         o_ctrl_out_valid
+    , input  logic                         i_ctrl_out_ready
+    // Mesh message streams
+    // - Inbound
+    , output node_message_t                o_mesh_in_data
+    , output logic                         o_mesh_in_valid
+    , input  logic                         i_mesh_in_ready
+    // - Outbound
+    , input  node_message_t                i_mesh_out_data
+    , input  logic                         i_mesh_out_valid
+    , output logic                         o_mesh_out_ready
     // Externally visible status
-    , output logic               o_status_active
-    , output logic               o_status_idle
-    , output logic               o_status_trigger
+    , output logic                         o_status_active
+    , output logic                         o_status_idle
+    , output logic                         o_status_trigger
     // Interface to the mesh
-    , input  logic [COLUMNS-1:0] i_mesh_idle
-    , output logic [COLUMNS-1:0] o_mesh_trigger
+    , input  logic [COLUMNS-1:0]           i_mesh_node_idle
+    , input  logic                         i_mesh_agg_idle
+    , output logic [COLUMNS-1:0]           o_mesh_trigger
+    , input  logic [(COLUMNS*OUTPUTS)-1:0] i_mesh_outputs
 );
 
 // =============================================================================
 // Constants
 // =============================================================================
 
-localparam RX_PYLD_WIDTH = MESSAGE_WIDTH - $bits(control_command_t);
-localparam TX_PYLD_WIDTH = MESSAGE_WIDTH;
+localparam MESH_OUTPUTS     = COLUMNS * OUTPUTS;
+localparam BITS_OUT_PER_MSG = 96;
+localparam MAX_OUTPUT_INDEX = (MESH_OUTPUTS + BITS_OUT_PER_MSG - 1) / BITS_OUT_PER_MSG;
+localparam OUTPUT_IDX_WIDTH = 1 + ((MESH_OUTPUTS > BITS_OUT_PER_MSG) ? $clog2(MAX_OUTPUT_INDEX) : 0);
+
+typedef enum logic [1:0] {
+      CTRL_IDLE    // 0: Quiescent state (mesh and controller idle)
+    , CTRL_ACTIVE  // 1: Controller active, trigger sent, waiting for mesh idle
+    , CTRL_OUTPUTS // 2: Generating output messages
+    , CTRL_MEMMORY // 3: Handling memory accesses
+} ctrl_state_t;
 
 // =============================================================================
 // Internal Signals and State
 // =============================================================================
 
-// Controller State
-`DECLARE_DQ(            1, soft_reset,     i_clk, i_rst, 'd0)
-`DECLARE_DQ(            1, seen_idle_low,  i_clk, i_rst, 'd0)
-`DECLARE_DQ(RX_PYLD_WIDTH, interval,       i_clk, i_rst, 'd0)
-`DECLARE_DQ(RX_PYLD_WIDTH, interval_count, i_clk, i_rst, 'd0)
-`DECLARE_DQ(            1, interval_set,   i_clk, i_rst, 'd0)
-`DECLARE_DQ(TX_PYLD_WIDTH, send_data,      i_clk, i_rst, 'd0)
-`DECLARE_DQ(            1, send_valid,     i_clk, i_rst, 'd0)
-`DECLARE_DQ(            1, all_idle,       i_clk, i_rst, 'd0)
+// FSM
+`DECLARE_DQT(ctrl_state_t, state, i_clk, i_rst, CTRL_IDLE)
+
+// Control request handling
+`DECLARE_DQ(          1, soft_reset,    i_clk, i_rst, 'd0)
+`DECLARE_DQ(TIMER_WIDTH, interval,      i_clk, i_rst, 'd0)
+`DECLARE_DQ(    COLUMNS, trigger_mask,  i_clk, i_rst, 'd0)
+`DECLARE_DQ(          1, active,        i_clk, i_rst, 'd0)
+
+logic              ctrl_req;
+control_req_type_t ctrl_cmd;
+logic              req_rd_params, req_rd_status, req_soft_rst, req_trigger,
+                   req_to_mesh;
+
+// Control response generation
+`DECLARE_DQT(control_response_t, ctrl_out_data,  i_clk, i_rst, 'd0)
+`DECLARE_DQ (1,                  ctrl_out_valid, i_clk, i_rst, 'd0)
+
+logic                         ctrl_out_stall;
+control_response_parameters_t resp_params;
+control_response_status_t     resp_status;
+control_response_outputs_t    resp_outputs;
+control_response_from_mesh_t  resp_mesh;
 
 // Trigger generation
-`DECLARE_DQ(      1, active,       i_clk, i_rst, 'd0)
-`DECLARE_DQ(      1, first_tick,   i_clk, i_rst, 'd1)
-`DECLARE_DQ(COLUMNS, trigger,      i_clk, i_rst, 'd0)
-`DECLARE_DQ(COLUMNS, trigger_mask, i_clk, i_rst, {COLUMNS{1'b1}})
+`DECLARE_DQ(      1, first_tick,    i_clk, i_rst, 'd1)
+`DECLARE_DQ(      1, seen_idle_low, i_clk, i_rst, 'd0)
+`DECLARE_DQ(      1, all_idle,      i_clk, i_rst, 'd1)
+`DECLARE_DQ(      1, trigger,       i_clk, i_rst, 'd0)
+`DECLARE_DQ(COLUMNS, trigger_cols,  i_clk, i_rst, 'd0)
 
-// Cycle counter
-`DECLARE_DQ(TX_PYLD_WIDTH, cycle, i_clk, i_rst, 'd0)
+// Cycle counting
+`DECLARE_DQ(TIMER_WIDTH, cycle, i_clk, i_rst, 'd0)
 
-// Inbound FIFO
-control_message_t ib_fifo_data;
-logic             ib_fifo_empty, ib_fifo_full, ib_fifo_push;
+// Output generation
+`DECLARE_DQ(OUTPUT_IDX_WIDTH, out_idx, i_clk, i_rst, 'd0)
 
-// Message decoding
-logic                     msg_stall, msg_next;
-logic                     is_cmd_param, is_cmd_active, is_cmd_status,
-                          is_cmd_cycles, is_cmd_interval, is_cmd_reset,
-                          is_cmd_trigmask;
-control_param_t           req_param;
-logic [TX_PYLD_WIDTH-1:0] resp_param, resp_cycles;
-control_status_t          resp_status;
+logic emit_output_msg;
 
 // Drive soft reset request
 assign o_soft_reset = soft_reset_q;
 
 // =============================================================================
-// Inbound Message FIFO
+// FSM
 // =============================================================================
 
-// Push when message presented and not full
-assign ib_fifo_push = i_inbound_valid && !ib_fifo_full;
+// Coalesce idle signals
+assign all_idle = (&i_mesh_node_idle) && i_mesh_agg_idle;
 
-// Deassert stream ready when FIFO is full
-assign o_inbound_ready = !ib_fifo_full;
+// Manage state transitions
+always_comb begin : comb_fsm
+    // Initialise
+    `INIT_D(state);
+    `INIT_D(out_idx);
 
-nx_fifo #(
-      .DEPTH     ( 2              )
-    , .WIDTH     ( MESSAGE_WIDTH  )
-) u_ib_fifo (
-      .i_clk     ( i_clk          )
-    , .i_rst     ( i_rst          )
-    // Write interface
-    , .i_wr_data ( i_inbound_data )
-    , .i_wr_push ( ib_fifo_push   )
-    // Read interface
-    , .o_rd_data ( ib_fifo_data   )
-    , .i_rd_pop  ( msg_next       )
-    // Status
-    , .o_level   (                )
-    , .o_empty   ( ib_fifo_empty  )
-    , .o_full    ( ib_fifo_full   )
-);
+    // State transitions
+    case (state_q)
+        // IDLE: Quiescent state waiting for activation
+        CTRL_IDLE : begin
+            if (active_q) begin
+                state = CTRL_ACTIVE;
+            end
+        end
+        // ACTIVE: Trigger sent, mesh active, waiting for idle
+        CTRL_ACTIVE : begin
+            if (seen_idle_low_q && all_idle_q) begin
+                state = CTRL_OUTPUTS;
+            end
+        end
+        // OUTPUTS: Generating output messages
+        CTRL_OUTPUTS : begin
+            if (!ctrl_out_stall) begin
+                // If on the final output message
+                if (out_idx_q == MAX_OUTPUT_INDEX[OUTPUT_IDX_WIDTH-1:0]) begin
+                    // Reset the output index
+                    out_idx = 'd0;
+                    // If still ticking go to ACTIVE, otherwise go to IDLE
+                    if (active_q) state = CTRL_ACTIVE;
+                    else          state = CTRL_IDLE;
 
-// =============================================================================
-// Message Handling
-// =============================================================================
-
-// Detect a stall
-assign msg_stall = o_outbound_valid && !i_outbound_ready;
-assign msg_next  = !msg_stall && !ib_fifo_empty;
-
-// Detect each message type
-assign is_cmd_param    = msg_next && (ib_fifo_data.raw.command == CONTROL_COMMAND_PARAM   );
-assign is_cmd_active   = msg_next && (ib_fifo_data.raw.command == CONTROL_COMMAND_ACTIVE  );
-assign is_cmd_status   = msg_next && (ib_fifo_data.raw.command == CONTROL_COMMAND_STATUS  );
-assign is_cmd_cycles   = msg_next && (ib_fifo_data.raw.command == CONTROL_COMMAND_CYCLES  );
-assign is_cmd_interval = msg_next && (ib_fifo_data.raw.command == CONTROL_COMMAND_INTERVAL);
-assign is_cmd_reset    = msg_next && (ib_fifo_data.raw.command == CONTROL_COMMAND_RESET   );
-assign is_cmd_trigmask = msg_next && (ib_fifo_data.raw.command == CONTROL_COMMAND_TRIGMASK);
-
-// Handle interval updates
-assign interval     = is_cmd_interval ? ib_fifo_data.raw.payload : interval_q;
-assign interval_set = is_cmd_interval ? (ib_fifo_data.raw.payload != 'd0) : interval_set_q;
-
-// Handle active
-assign active = (
-    is_cmd_active
-        ? ib_fifo_data.active.active
-        : ((interval_set_q && interval_count_q == interval_q) ? 'd0 : active_q)
-);
-
-// Handle interval counting
-assign interval_count = (
-    (is_cmd_interval || (interval_set_q && interval_count_q == interval_q))
-        ? 'd0
-        : interval_count_q + ((|trigger_q) ? 'd1 : 'd0)
-);
-
-// Handle soft reset
-assign soft_reset = is_cmd_reset ? ib_fifo_data.raw.payload[0] : soft_reset_q;
-
-// Handle trigger mask
-assign trigger_mask = is_cmd_trigmask ? ib_fifo_data.raw.payload[COLUMNS-1:0] : trigger_mask_q;
-
-// Build parameter response
-assign req_param  = ib_fifo_data.param.param;
-assign resp_param = (
-    (req_param == CONTROL_PARAM_ID            ) ? { {(MESSAGE_WIDTH-24){1'b0}}, HW_DEV_ID[23:0] } :
-    (req_param == CONTROL_PARAM_VERSION       ) ? { {(MESSAGE_WIDTH-16){1'b0}}, HW_VER_MAJOR[7:0], HW_VER_MINOR[7:0] } :
-    (req_param == CONTROL_PARAM_COUNTER_WIDTH ) ? TX_PYLD_WIDTH[TX_PYLD_WIDTH-1:0] :
-    (req_param == CONTROL_PARAM_ROWS          ) ? ROWS[TX_PYLD_WIDTH-1:0] :
-    (req_param == CONTROL_PARAM_COLUMNS       ) ? COLUMNS[TX_PYLD_WIDTH-1:0] :
-    (req_param == CONTROL_PARAM_NODE_INPUTS   ) ? INPUTS[TX_PYLD_WIDTH-1:0] :
-    (req_param == CONTROL_PARAM_NODE_OUTPUTS  ) ? OUTPUTS[TX_PYLD_WIDTH-1:0] :
-    (req_param == CONTROL_PARAM_NODE_REGISTERS) ? REGISTERS[TX_PYLD_WIDTH-1:0]
-                                                : 'd0
-);
-
-// Build status response
-assign resp_status.active       = active_q;
-assign resp_status.idle_low     = seen_idle_low_q;
-assign resp_status.first_tick   = first_tick_q;
-assign resp_status.interval_set = interval_set_q;
-assign resp_status._padding_0   = 'd0;
-
-// Build cycle response
-assign resp_cycles = cycle_q;
-
-// Select the correct response
-assign send_data = (
-    msg_stall ? send_data_q
-              : (is_cmd_param  ? resp_param  :
-                 is_cmd_status ? resp_status :
-                 is_cmd_cycles ? resp_cycles
-                               : 'd0)
-);
-assign send_valid = (is_cmd_param || is_cmd_status || is_cmd_cycles || msg_stall);
-
-// Drive outputs with send data
-assign o_outbound_data  = send_data_q;
-assign o_outbound_valid = send_valid_q;
+                // Otherwise count up
+                end else begin
+                    out_idx = (out_idx_q + 'd1);
+                end
+            end
+        end
+        // Default: Return to IDLE
+        default: begin
+            state = CTRL_IDLE;
+        end
+    endcase
+end
 
 // =============================================================================
 // Trigger Generation
 // =============================================================================
 
-// Create a summary of column idle state
-assign all_idle = &i_mesh_idle;
+// Generate trigger on transition into ACTIVE
+assign trigger        = (state_q != CTRL_ACTIVE) && (state == CTRL_ACTIVE);
+assign trigger_cols   = {COLUMNS{trigger}} & trigger_mask_q;
+assign o_mesh_trigger = trigger_cols_q;
 
-// Monitor for idle going low after each trigger event
-assign seen_idle_low = (|trigger) ? 1'b0 : (seen_idle_low_q || !all_idle_q);
+// Monitor for idle going low after trigger
+assign seen_idle_low = trigger ? 'd0 : (seen_idle_low_q || !all_idle_q);
 
-// Generate column triggers
-assign trigger = {COLUMNS{active_q && seen_idle_low_q && all_idle_q}} & trigger_mask_q;
-
-// Track the first trigger into the mesh
-assign first_tick = first_tick_q && (trigger == 'd0);
-
-// Drive mesh trigger
-assign o_mesh_trigger = trigger_q;
+// Track the very first tick
+assign first_tick = first_tick_q && !trigger;
 
 // =============================================================================
 // Cycle Counter
 // =============================================================================
 
-assign cycle = (|trigger) ? (cycle_q + { {(TX_PYLD_WIDTH-1){1'b0}}, 1'b1 }) : cycle_q;
+assign cycle = cycle_q + ((state_q == CTRL_OUTPUTS && state != CTRL_OUTPUTS) ? 'd1 : 'd0);
+
+// =============================================================================
+// Control Request Handling
+// =============================================================================
+
+// Qualify request
+assign ctrl_req = i_ctrl_in_valid && o_ctrl_in_ready;
+
+// Extract command
+assign ctrl_cmd = i_ctrl_in_data.raw.command;
+
+// Decode
+assign req_rd_params = ctrl_req && (ctrl_cmd == CONTROL_REQ_TYPE_READ_PARAMS);
+assign req_rd_status = ctrl_req && (ctrl_cmd == CONTROL_REQ_TYPE_READ_STATUS);
+assign req_soft_rst  = ctrl_req && (ctrl_cmd == CONTROL_REQ_TYPE_SOFT_RESET );
+assign req_trigger   = ctrl_req && (ctrl_cmd == CONTROL_REQ_TYPE_TRIGGER    );
+assign req_to_mesh   = ctrl_req && (ctrl_cmd == CONTROL_REQ_TYPE_TO_MESH    );
+
+// Trigger reset
+assign soft_reset = req_soft_rst || soft_reset_q;
+
+// Capture column triggering mask
+assign trigger_mask = req_trigger ? i_ctrl_in_data.trigger.col_mask[COLUMNS-1:0]
+                                  : trigger_mask_q;
+
+// Set interval on request, count down on trigger if greater than 0
+assign interval = req_trigger                   ? i_ctrl_in_data.trigger.cycles :
+                  (trigger && interval_q > 'd0) ? (interval_q - 'd1)
+                                                : interval_q;
+
+// Set active on request, deactivate on trigger if interval is 1
+assign active = req_trigger                    ? i_ctrl_in_data.trigger.active :
+                (trigger && interval_q == 'd1) ? 'd0
+                                               : active_q;
+
+// Forward messages into the mesh
+assign o_mesh_in_data  = i_ctrl_in_data.to_mesh.message;
+assign o_mesh_in_valid = req_to_mesh;
+
+// Drive ready signal
+assign o_ctrl_in_ready = i_mesh_in_ready && (state_q != CTRL_OUTPUTS);
+
+// =============================================================================
+// Control Response Generation
+// =============================================================================
+
+// Pad outputs with zeroes up to the next 96 bit boundary
+localparam FULL_WIDTH = MAX_OUTPUT_INDEX * BITS_OUT_PER_MSG;
+localparam OUTPUT_PAD = FULL_WIDTH - MESH_OUTPUTS;
+logic [FULL_WIDTH-1:0] padded_outputs;
+assign padded_outputs = { OUTPUT_PAD'(0), i_mesh_outputs };
+
+// Populate output response
+assign emit_output_msg = (state_q == CTRL_OUTPUTS) && (state == CTRL_OUTPUTS);
+
+always_comb begin : comb_outputs
+    resp_outputs.format = CONTROL_RESP_TYPE_OUTPUTS;
+    resp_outputs.stamp  = cycle_q;
+    resp_outputs.index  = {
+        {($clog2(MESH_OUTPUTS / BITS_OUT_PER_MSG)-OUTPUT_IDX_WIDTH+1){1'b0}},
+        out_idx_q
+    };
+    resp_outputs._padding_0 = 'd0;
+    for (int idx = 0; idx < MAX_OUTPUT_INDEX; idx++) begin : gen_outputs
+        if (out_idx_q == idx[OUTPUT_IDX_WIDTH-1:0]) begin
+            assign resp_outputs.section = padded_outputs[idx*BITS_OUT_PER_MSG+:BITS_OUT_PER_MSG];
+        end
+    end
+end
+
+// Populate forwarded mesh response
+assign resp_mesh.format     = CONTROL_RESP_TYPE_FROM_MESH;
+assign resp_mesh.message    = i_mesh_out_data;
+assign resp_mesh._padding_0 = 'd0;
+assign o_mesh_out_ready     = !ctrl_out_stall && (state_q != CTRL_OUTPUTS);
+
+// Populate parameter response
+assign resp_params.format      = CONTROL_RESP_TYPE_PARAMS;
+assign resp_params.id          = HW_DEV_ID;
+assign resp_params.ver_major   = HW_VER_MAJOR;
+assign resp_params.ver_minor   = HW_VER_MINOR;
+assign resp_params.timer_width = TIMER_WIDTH;
+assign resp_params.rows        = ROWS[$clog2(MAX_ROW_COUNT)-1:0];
+assign resp_params.columns     = COLUMNS[$clog2(MAX_COLUMN_COUNT)-1:0];
+assign resp_params.node_ins    = INPUTS[$clog2(MAX_NODE_INPUTS)-1:0];
+assign resp_params.node_outs   = OUTPUTS[$clog2(MAX_NODE_OUTPUTS)-1:0];
+assign resp_params.node_regs   = REGISTERS[$clog2(MAX_NODE_REGISTERS)-1:0];
+assign resp_params._padding_0  = 'd0;
+
+// Populate status response
+assign resp_status.format     = CONTROL_RESP_TYPE_STATUS;
+assign resp_status.active     = active_q;
+assign resp_status.mesh_idle  = &i_mesh_node_idle;
+assign resp_status.agg_idle   = i_mesh_agg_idle;
+assign resp_status.seen_low   = seen_idle_low_q;
+assign resp_status.first_tick = first_tick_q;
+assign resp_status.cycle      = cycle_q;
+assign resp_status.countdown  = interval_q;
+assign resp_status._padding_0 = 'd0;
+
+// Determine when to hold the response
+assign ctrl_out_stall = ctrl_out_valid_q && !i_ctrl_out_ready;
+
+// Mux between responses
+assign ctrl_out_data = ctrl_out_stall   ? ctrl_out_data_q :
+                       emit_output_msg  ? resp_outputs    :
+                       i_mesh_out_valid ? resp_mesh       :
+                       req_rd_params    ? resp_params     :
+                       req_rd_status    ? resp_status
+                                        : 'd0;
+
+// Drive the valid
+assign ctrl_out_valid = (
+    ctrl_out_stall   ||
+    req_rd_params   ||
+    req_rd_status   ||
+    emit_output_msg ||
+    i_mesh_out_valid
+);
+
+// Drive the ports
+assign o_ctrl_out_data  = ctrl_out_data_q;
+assign o_ctrl_out_valid = ctrl_out_valid_q;
 
 // =============================================================================
 // Debug Status Flags
 // =============================================================================
 
-assign o_status_active  = active_q;
-assign o_status_idle    = all_idle_q;
-assign o_status_trigger = |trigger_q;
+assign o_status_active  = (state_q != CTRL_IDLE);
+assign o_status_idle    = (state_q == CTRL_IDLE);
+assign o_status_trigger = trigger_q;
 
 endmodule : nx_control
