8be4ab4db577dcac12412c60a9d444cc116d1b11
Peter Birch <peter@lightlogic.co.uk>
2023-01-29 16:26:20 +0000

    Updating copyright date from 2021 to 2023

--- nxmodel/py/test.sh
+++ nxmodel/py/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2021, Peter Birch, mailto:peter@lightlogic.co.uk
+# Copyright 2023, Peter Birch, mailto:peter@lightlogic.co.uk
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
